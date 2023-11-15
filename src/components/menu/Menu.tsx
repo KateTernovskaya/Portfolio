92a83143e5ec355d7fa9ae55a939f2f4e75c7670
Kate <kate-ternovskaya99@mail.ru>
2023-11-15 14:51:24 +0500

    start project

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Menu = () => {
+    return (
+        <StyledMenu>
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Contacts</a></li>
+            </ul>
+        </StyledMenu>
+    );
+};
+
+const StyledMenu = styled.nav`
+  ul {
+    display: flex;
+    gap: 50px;
+  }
+
+  li {
+    padding: 5px;
+  }
+
+  & li:hover {
+    transition: all .5s ease;
+    border-bottom: 1px solid rgba(6,49,131,0.5);
+    color: rgba(6,49,131,0.5);
+  }
+`
+
